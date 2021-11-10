4199a7a3b29dcfd3b5ed38f938c817badf262752
Ester Gama <esterfaniasantos@gmail.com>
2021-11-10 12:52:01 -0300

    chore(boilerplate): add editor config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 type HomeProps = {
   title: string;
 };
 
-export default function Home({ title = 'React Avançado' }: HomeProps) {
+export default function Home({ title = "React Avançado" }: HomeProps) {
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <meta name='description' content='React Avançado' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="React Avançado" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
   );
