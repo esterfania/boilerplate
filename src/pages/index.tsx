d1eb2f91c686b6347fca9ec226835febecc315b5
Ester Gama <esterfaniasantos@gmail.com>
2021-11-11 18:21:16 -0300

    chore(boilerplate): add lint script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import Head from "next/head";
 
 type HomeProps = {
-  title: string;
+    title: string;
 };
 
 export default function Home({ title = "React Avançado" }: HomeProps) {
-  return (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content="React Avançado" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  );
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content="React Avançado" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+    );
 }
