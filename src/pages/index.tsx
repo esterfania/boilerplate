008d8d1cc2f3b71fac7fe784a8760928996468c0
Ester Gama <esterfaniasantos@gmail.com>
2021-11-12 16:49:31 -0300

    chore(boilerplate): add styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,5 @@
-import Head from 'next/head';
+import Main from 'components/Main';
 
-type HomeProps = {
-    title: string;
-};
-
-export default function Home({ title = 'React Avançado' }: HomeProps) {
-    return (
-        <div>
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content="React Avançado" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-        </div>
-    );
+export default function Home() {
+    return <Main />;
 }
